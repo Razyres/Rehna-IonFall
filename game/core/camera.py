705ec20439ff0fcec3273753300ffdc9ec796059
alexandre.vandenberghe <alexandre.vanenberghe@epita.fr>
2026-01-16 12:50:22 +0100

    Camera started

---
+++ game/core/camera.py
@@ -0,0 +1,32 @@
+import pygame
+
+class Camera():
+    def __init__(self, width, height, map_width, map_height):
+        self.width = width
+        self.height = height
+        self.map_width = map_width
+        self.map_height = map_height
+        self.x = 0
+        self.y = 0
+        self.zoom = 1.5
+        self.target_zoom = 1.5
+        self.smoothing = 0.1
+        
+    def follow(self, target_rect):
+        target_x = target_rect.centerx - (self.width / 2) / self.zoom
+        target_y = target_rect.centery - (self.height / 2) / self.zoom
+        self.x = (target_x - self.x) / self.smoothing
+        self.y = (target_y - self.y) / self.smoothing
+        self.x = max(0, min(self.x, self.map_width - self.width / self.zoom))
+        self.y = max(0, min(self.y, self.map_height - self.height / self.zoom))
+    
+    def apply(self, rect):
+        return pygame.Rect(
+            (rect.x - self.x) * self.zoom,
+            (rect.y - self.y) * self.zoom,
+            self.width * self.zoom,
+            self.height * self.zoom
+        )
+    
+    def apply_():
+        pass
