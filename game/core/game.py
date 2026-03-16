da6bc36a099268c17752b07cf080cac5bf44b531
alexandre.vandenberghe <alexandre.vanenberghe@epita.fr>
2026-03-16 17:55:00 +0100

    NICO LA SALOLOPUREE

--- game/core/game.py
+++ game/core/game.py
@@ -99,9 +99,9 @@ enemy_sprite = pygame.transform.scale(enemy, (52, 94))
 enemy = Enemy(400, 400, 52, 94, enemy_sprite, 40) 
 game.add_entity(enemy)
 
-enemy2 = pygame.image.load("sprite/Bunyon_S.jpg")
+enemy2 = pygame.image.load("sprite/Bunyon_S.png")
 enemy2_sprite = pygame.transform.scale(enemy2, (42, 102))
-enemy2 = Enemy(600, 600, 43, 102, enemy2_sprite, 20)
+enemy2 = Enemy(620, 620, 43, 102, enemy2_sprite, 20)
 game.add_entity(enemy2)
 game.run()
 
